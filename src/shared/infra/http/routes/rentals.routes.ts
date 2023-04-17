54f7798fbdb87d4121a105f31f61e471edffe233
Gabriel Santana <gabrielsantana100@hotmail.com>
2023-04-17 14:26:34 -0300

    Devolution Rental

--- src/shared/infra/http/routes/rentals.routes.ts
+++ src/shared/infra/http/routes/rentals.routes.ts
@@ -1,11 +1,14 @@
 import { CreateRentalController } from "@modules/rentals/useCases/createRental/CreateRentalController";
 import { Router } from "express";
 import { ensureAuthenticaticated } from "../middlewares/ensureAuthenticated";
+import { DevolutionRentalController } from "@modules/rentals/useCases/devolutionRental/DevolutionRentalController";
 
 const rentalRoutes = Router()
 
 const createRentalController = new CreateRentalController()
+const devolutionRentalController = new DevolutionRentalController()
 
 rentalRoutes.post("/", ensureAuthenticaticated, createRentalController.handle)
+rentalRoutes.post("/devolution/:id", ensureAuthenticaticated, devolutionRentalController.handle)
 
 export { rentalRoutes }
