2322b6e2a775a6f927ffb47e86f21f8101a63bf0
Gabriel Santana <gabrielsantana100@hotmail.com>
2023-05-05 10:41:34 -0300

    refreshToken fix

--- src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
+++ src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
@@ -9,6 +9,11 @@ interface IPayload {
   sub: string
   email: string
 }
+
+interface ITokenResponse {
+  token: string
+  refresh_token: string
+}
 @injectable()
 class RefreshTokenUseCase {
   constructor(
@@ -20,7 +25,7 @@ class RefreshTokenUseCase {
 
   }
 
-  async execute(token: string): Promise<string> {
+  async execute(token: string): Promise<ITokenResponse> {
     const { email, sub } = verify(token, auth.secret_refresh_token) as IPayload
 
     const user_id = sub
@@ -48,7 +53,15 @@ class RefreshTokenUseCase {
 
     })
 
-    return refresh_token
+    const newToken = sign({}, auth.secret_token, {
+      subject: user_id,
+      expiresIn: auth.expires_in_token
+    })
+
+    return {
+      refresh_token,
+      token: newToken
+    }
 
   }
 }
--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -1,8 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
 import { AppError } from "@shared/errors/AppError";
-import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
-import { UsersTokensRepository } from "@modules/accounts/infra/typeorm/repositories/UsersTokensRepository";
 import auth from "@config/auth";
 
 
@@ -12,7 +10,6 @@ interface IPayload {
 export async function ensureAuthenticaticated(request: Request, response: Response, next: NextFunction) {
 
   const authHeader = request.headers.authorization
-  const userTokensRepository = new UsersTokensRepository()
 
   if (!authHeader) {
     throw new AppError("Token missing", 401)
